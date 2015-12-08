ba6b02af1f723bf2c8592fa9c38192cfa86a62ba
hxx0215 <hxx0215@126.com>
2015-12-08 21:57:52 +0800

    健康日报UI

---
+++ 工程/meizhikang/meizhikang/View/HealthPercentView.swift
@@ -0,0 +1,13 @@
+//
+//  HealthPercentView.swift
+//  meizhikang
+//
+//  Created by shadowPriest on 15/12/8.
+//  Copyright © 2015年 刘向宏. All rights reserved.
+//
+
+import UIKit
+
+class HealthPercentView: UIView {
+
+}
