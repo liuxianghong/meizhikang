0b909cdb1470cb1acd8b13130e2d2551611470e4
liuxh532 <786995209@qq.com>
2015-12-15 23:06:51 +0800

    注册hud

--- 工程/meizhikang/meizhikang/ViewController/Login/RegistTableViewController.swift
+++ 工程/meizhikang/meizhikang/ViewController/Login/RegistTableViewController.swift
@@ -123,7 +123,7 @@ class RegistTableViewController: UITableViewController {
             print(token)
             print(time)
             IMConnect.Instance().registWithToken(token, withCode: "1234", withNickName: self.nickNameTextField.text, withPW: "123456", withSex: sex, withAge: age!, withHeiht: height!, withWight: wight!, completion: { () -> Void in
-                hud.mode = .Text
+                //hud.mode = .Text
                 hud.detailsLabelText = "注册成功，正在获取用户信息";
                 let dic : NSDictionary = ["type": "account"]
                 IMConnect.Instance().RequstUserInfo(dic as [NSObject : AnyObject], completion: { (object) -> Void in
