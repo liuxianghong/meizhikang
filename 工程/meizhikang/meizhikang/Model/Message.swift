810ef025bc2bab146e10ee8ecec4932d0f9ee80c
hxx0215 <hxx0215@126.com>
2015-12-20 22:56:41 +0800

    Message结构

---
+++ 工程/meizhikang/meizhikang/Model/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  meizhikang
+//
+//  Created by shadowPriest on 15/12/20.
+//  Copyright © 2015年 刘向宏. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Message: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
